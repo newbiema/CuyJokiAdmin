389dd4d04ab80875b128f932c4ab1bbc87709f15
SandraMAP <sandraaamap@gmail.com>
2024-10-21 13:49:04 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Karyawan Cuy Joki</title>
+    <title>Data Karyawan CuyJoki</title>
     <link rel="stylesheet" href="css/output.css">
 </head>
 <body class="bg-gray-100">
